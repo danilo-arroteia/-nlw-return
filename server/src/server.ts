b67f03490ca6a80028f131c53f9feaae3aecab67
Your Name <you@example.com>
2022-05-05 17:40:28 -0300

    55:13 NLW Return - Stage 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,49 @@
 import express from "express";
+import nodemailer from 'nodemailer';
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
-// GET, POST, PUT, PATCH, DELETE
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "660e401c1cd55a",
+    pass: "601d9e9f545302"
+  }
+});
 
-// GET = Buscar informações
-// POST = Cadastrar informações
-// PUT = Atualizar informações de uma entidade
-// PATCH = Atualizar uma informação única de um entidade
-// DELETE = Deletar uma informação
-
-app.post("/feedbacks", async (req, res) => {
+app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const feedback = await prisma.feedback.create({
     data: {
       type,
       comment,
-      screenshot,
+      screenshot, 
     },
-  });
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Danilo Arroteia <sikor12@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Tipo do comentario: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+
+  })
 
   return res.status(201).json({ data : feedback});
 });
 
 app.listen(3333, () => {
-  console.log("HTTP server running! on port");
+  console.log("HTTP server running!");
 });
 
 // SQLite ( Não precisa instalar nada, salva como arquivo físico )
